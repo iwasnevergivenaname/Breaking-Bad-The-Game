3b7f7ab3ea578612000468b121720e82621bdf60
iwasnevergivenaname <hannah.mace93@gmail.com>
2020-07-25 14:51:43 -0700

    [style.css app.js] connecting on screen items to DOM elements through click listeners

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
 const h1 = document.querySelector("h1");
+const hazmatSuit = document.getElementById("hazmat");
+const notebook = document.getElementById("notebook");
+const isoAlc = document.getElementById("isopropyl-alc");
+
+console.log(hazmatSuit);
+console.log(notebook);
+console.log(isoAlc);
+
 h1.addEventListener("click", function() {
     console.log("it was clicked");
 })
+hazmatSuit.addEventListener("click", function() {
+    console.log("you clicked the suit");
+})
+notebook.addEventListener("click", function ()  {
+    console.log("you clicked the notebook");
+})
+
+isoAlc.addEventListener("click", function () {
+    console.log("you clicked the isopropyl alcohol");
+})
+
 
